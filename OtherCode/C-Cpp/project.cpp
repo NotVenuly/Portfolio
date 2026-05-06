eff6fc3f591eeea67044c676bd1e4228734f7341
NotVenuly <147390259+NotVenuly@users.noreply.github.com>
2026-05-06 18:15:37 +0300

    Updated git ignore

--- OtherCode/C-Cpp/project.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main() {
-    std::string title = "Physics Prototype";
-    std::cout << "Project: " << title << "\n";
-    std::cout << "This sample code is a placeholder for a C/C++ game systems project." << std::endl;
-    return 0;
-}
--- OtherCode/Go/project.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Go project placeholder for portfolio")
-	fmt.Println("Use this file to demonstrate a Go game server tool or build utility.")
-}
